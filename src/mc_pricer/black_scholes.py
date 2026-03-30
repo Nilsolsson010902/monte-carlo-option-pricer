008606ea8d69c586c79c0dc8e73331c2983a64d9
Nilsolsson010902 <nilsjohn6@gmail.com>
2026-03-30 15:13:28 +0200

    greeks added to black scholes and some refactoring

--- src/mc_pricer/black_scholes.py
+++ src/mc_pricer/black_scholes.py
@@ -7,14 +7,38 @@ class BlackScholes:
     
     @staticmethod
     def norm_cdf(x:float ) -> float:
-        # Standard normal CDF via error function
+        """
+        The Normal Cumulative Distribution Function (CDF) calculates the probability that a normally 
+        distributed random variable is less than or equal to a specific value x. 
+        """
         return 0.5 * (1.0 + math.erf(x / math.sqrt(2.0)))
+    
+    @staticmethod
+    def norm_pdf(x: float) -> float:
+        """
+        The normal probability density function (PDF) is a formula defining the bell-shaped curve of 
+        continuous data centered around a mean. 
+        """
+        return math.exp(-0.5 * x**2) / math.sqrt(2.0 * math.pi)
+    
+    @staticmethod
+    def d1(S0: float, K: float, T: float, r: float, sigma: float) -> float:
+        return (math.log(S0 / K) + (r + 0.5 * sigma**2) * T) / (sigma * math.sqrt(T))
 
     @staticmethod
-    def black_scholes_price(S0: float, K: float, T: float, r: float, sigma: float, option_type: str) -> float:
+    def d2(S0: float, K: float, T: float, r: float, sigma: float) -> float:
+        return BlackScholes.d1(S0, K, T, r, sigma) - sigma * math.sqrt(T)
+
+    @staticmethod
+    def black_scholes_price(S0: float, 
+                            K: float, 
+                            T: float, 
+                            r: float, 
+                            sigma: float, 
+                            option_type: str) -> float:
         """
             Black–Scholes price for a European option.
-            Inputs:
+            Parameters:
             S0: spot price today
             K: strike
             T: time to maturity in years (e.g. 0.5 = 6 months)
@@ -30,12 +54,11 @@ class BlackScholes:
             else:
                 raise ValueError("option_type must be 'call' or 'put'")
             
-    
         if sigma <= 0:
             raise ValueError("sigma must be > 0")
             
-        d1 = (math.log(S0/K) + (r + sigma**2 * 0.5 ) * T)/(sigma*math.sqrt(T))
-        d2 = d1 - sigma*math.sqrt(T)
+        d1 = BlackScholes.d1(S0, K, T, r, sigma)
+        d2 = BlackScholes.d2(S0, K, T, r, sigma)
 
         if option_type.lower() == "call":
             return S0*BlackScholes.norm_cdf(d1) - K*BlackScholes.norm_cdf(d2)*math.exp(-r * T)
@@ -45,7 +68,90 @@ class BlackScholes:
 
         else:
             raise ValueError("option_type must be 'call' or 'put'")
+        
+
+    @staticmethod
+    def delta(S0: float,
+              K: float,
+              T: float,
+              r: float,
+              sigma: float,
+              option_type: str
+              )-> float:
+        """ 
+        Delta greek for european call option.
+        Measures relationship between options price and underlying stock. 
+        Parameters:
+        S0: spot price. 
+        K: strike price.
+        T: time to maturity in years. 
+        r: continuously compounded risk-free rate. 
+        sigma: volatility (e.g. 0.2). 
+        option_type: "call" or "put".
+        """
+
+        if T <= 0:
+            if option_type.lower() == "call":
+                return 1.0 if S0 > K else 0.0
+            elif option_type.lower() == "put":
+                return -1.0 if S0 < K else 0.0
+            else:
+                raise ValueError("No such option type")
+            
+        d1 = BlackScholes.d1(S0, K, T, r, sigma)
+
+        if option_type.lower() == "call":
+            return BlackScholes.norm_cdf(d1)
+        elif option_type.lower() == "put":
+            return BlackScholes.norm_cdf(d1) - 1.0
+        else:
+            raise ValueError("No such option type")
+        
+    def vega(S0: float,
+              K: float,
+              T: float,
+              r: float,
+              sigma: float,
+              )-> float:
+        """ 
+        Vega greek for european call option.
+        Measures the option's price sensitivity to changes in the underlying asset's implied volatility
+        Parameters:
+        S0: spot price. 
+        K: strike price.
+        T: time to maturity in years. 
+        r: continuously compounded risk-free rate. 
+        sigma: volatility (e.g. 0.2). 
+        """
+        if T <= 0:
+            return 0.0
+
+        d1 = BlackScholes.d1(S0, K, T, r, sigma)
+        return S0 * BlackScholes.norm_pdf(d1) * math.sqrt(T)
+
+    def gamma(S0: float,
+              K: float,
+              T: float,
+              r: float,
+              sigma: float,
+              )-> float:
+        """ 
+        Gamma greek for european call option.
+        Gamma measures the sensitivity of the option's delta.
+        Parameters:
+        S0: spot price. 
+        K: strike price.
+        T: time to maturity in years. 
+        r: continuously compounded risk-free rate. 
+        sigma: volatility (e.g. 0.2). 
+        """
+        if T <= 0:
+            return 0.0
+
+        d1 = BlackScholes.d1(S0, K, T, r, sigma)
+        return BlackScholes.norm_pdf(d1) / (S0 * sigma * math.sqrt(T))
+
 
 if __name__ == "__main__":
     print(BlackScholes.black_scholes_price(100, 95, 0.25, 0.1 , 0.5, "call"))
-    print(MonteCarlo.mc_simulator(100000, 100, 95, 0.25, 0.1 , 0.5, "call"))
+  
